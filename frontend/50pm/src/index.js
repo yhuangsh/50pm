15f7bc3f66cbfe2ef9136694cb58c294daf81a60
Yong Huang <yong.huang.sh@gmail.com>
2019-03-31 22:16:36 +0800

    Add CNZZ analytics

--- frontend/50pm/src/index.js
+++ frontend/50pm/src/index.js
@@ -10,3 +10,15 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+console.log('process.env = ', process.env);
+
+let cnzz_protocol = (("https:" === document.location.protocol) ? "https://" : "http://");
+
+document.write(
+  "<span style='display: none' id='cnzz_stat_icon_" + process.env.REACT_APP_CNZZ_ID + "'></span>" + 
+  "<script " + 
+      "src='" + cnzz_protocol + "s5.cnzz.com/z_stat.php?id=" + process.env.REACT_APP_CNZZ_ID + "&show=pic1' " + 
+      "type='text/javascript'>" + 
+  "</script>"
+);
