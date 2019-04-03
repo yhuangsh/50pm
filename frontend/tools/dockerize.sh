1415cd268db504d1b4d1292c649b5a225765be0a
Yong Huang <yong.huang.sh@gmail.com>
2019-04-03 21:32:55 +0800

    Add secret docker io credentials

--- frontend/tools/dockerize.sh
+++ frontend/tools/dockerize.sh
@@ -4,4 +4,5 @@
 cp config/nginx.default.conf 50pm/build
 cp tools/Dockerfile.nginx-static 50pm/build
 docker build -t yhuangsh/50pm -f 50pm/build/Dockerfile.nginx-static 50pm/build
-docker push yhuangsh/50pm
+docker login $DOCKER_IO_CREDENTIAL
+docker push yhuangsh/50pm 
