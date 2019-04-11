a684e081ead54779e7aed462c0349fb32930458d
Yong Huang <yong.huang.sh@gmail.com>
2019-04-11 08:32:07 +0800

    Pull latest image from docker.io

--- frontend/tools/deploy.production.sh
+++ frontend/tools/deploy.production.sh
@@ -5,6 +5,7 @@ then
   echo "missing release tag name"
   exit
 else
+  docker pull yhuangsh/50pm:latest
   docker tag yhuangsh/50pm:latest yhuangsh/50pm:$1
   docker push yhuangsh/50pm:$1
   kubectl set image deploy/fiftypm fiftypm=yhuangsh/50pm:$1 --record
