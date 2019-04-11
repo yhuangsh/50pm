88e2e8d6d1b2e675b45c6ea7efac82e3ad289d10
Yong Huang <yong.huang.sh@gmail.com>
2019-04-12 01:46:45 +0800

    Try to build again

--- frontend/tools/deploy.production.sh
+++ frontend/tools/deploy.production.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 # On Travis, must run this script after dockerize & deploy.staging
-if [ -n $TRAVIS_TAG ] 
+
+if [ -z $TRAVIS_TAG ] 
 then
+  echo "This is not a release build, skipping production deployment"
+else
   docker tag yhuangsh/50pm:latest yhuangsh/50pm:$TRAVIS_TAG
   docker push yhuangsh/50pm:$TRAVIS_TAG
   ./kubectl --kubeconfig=./kc set image deploy/fiftypm fiftypm=yhuangsh/50pm:$TRAVIS_TAG --record
-else
-  echo "This is not a release build, skipping production deployment"
 fi
