ffcdb8a6d4403ed3bd5940a990532a514b116549
Yong Huang <yong.huang.sh@gmail.com>
2019-04-14 18:11:20 +0800

    Rename due to hostname length limited to 64 bytes

--- backend/tools/dev0.sh
+++ backend/tools/dev0.sh
@@ -2,13 +2,13 @@
 docker run \
     --rm \
     --net dev-net \
-    --name fiftypm_api-staging-0 \
+    --name fiftypm_api-stg-0 \
     --ip 172.28.0.12 \
-    --hostname fiftypm_api-staging-0.fiftypm_api-staging.default.svc.cluster.local \
-    --add-host fiftypm_api-staging-0.fiftypm_api-staging.default.svc.cluster.local:172.28.0.12 \
-    --add-host fiftypm_api-staging-1.fiftypm_api-staging.default.svc.cluster.local:172.28.0.13 \
-    --add-host fiftypm_api-staging-2.fiftypm_api-staging.default.svc.cluster.local:172.28.0.14 \
+    --hostname fiftypm_api-stg-0.fiftypm_api-stg.default.svc.cluster.local \
+    --add-host fiftypm_api-stg-0.fiftypm_api-stg.default.svc.cluster.local:172.28.0.12 \
+    --add-host fiftypm_api-stg-1.fiftypm_api-stg.default.svc.cluster.local:172.28.0.13 \
+    --add-host fiftypm_api-stg-2.fiftypm_api-stg.default.svc.cluster.local:172.28.0.14 \
     -v `pwd`:/project \
-    -p 8000:8000 \
+    -p 8000:80 \
     -it \
     yhuangsh/dev-alpine-erlang:latest
