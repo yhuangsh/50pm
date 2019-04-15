d12167261f7242b83cff088b85f1c295120dd3f4
Yong Huang <yong.huang.sh@gmail.com>
2019-04-15 22:56:57 +0800

    Fix long name

--- backend/tools/dev2.sh
+++ backend/tools/dev2.sh
@@ -2,12 +2,12 @@
 docker run \
     --rm \
     --net dev-net \
-    --name fiftypm_api-staging-2 \
+    --name fiftypm_api-stg-2 \
     --ip 172.28.0.14 \
-    --hostname fiftypm_api-staging-2.fiftypm_api-staging.default.svc.cluster.local \
-    --add-host fiftypm_api-staging-0.fiftypm_api-staging.default.svc.cluster.local:172.28.0.12 \
-    --add-host fiftypm_api-staging-1.fiftypm_api-staging.default.svc.cluster.local:172.28.0.13 \
-    --add-host fiftypm_api-staging-2.fiftypm_api-staging.default.svc.cluster.local:172.28.0.14 \
+    --hostname fiftypm_api-stg-2.fiftypm_api-stg.default.svc.cluster.local \
+    --add-host fiftypm_api-stg-0.fiftypm_api-stg.default.svc.cluster.local:172.28.0.12 \
+    --add-host fiftypm_api-stg-1.fiftypm_api-stg.default.svc.cluster.local:172.28.0.13 \
+    --add-host fiftypm_api-stg-2.fiftypm_api-stg.default.svc.cluster.local:172.28.0.14 \
     -v `pwd`:/project \
     -p 8002:8000 \
     -it \
