4f1ff0d9ee7645de8cdd0bb0dea4de57b8e46040
Yong Huang <yong.huang.sh@gmail.com>
2019-04-25 23:04:59 +0800

    Finish github callback and get access token

--- backend/fiftypm_api/src/fiftypm_api_login.erl
+++ backend/fiftypm_api/src/fiftypm_api_login.erl
@@ -6,14 +6,21 @@
     ]
 ).
 
--define(GITHUB_AUTH_URL_PRE, <<"https://github.com/login/oauth/authorize?">>).
+-define(GITHUB_AUTH_URL, "https://github.com/login/oauth/authorize?").
+-define(GITHUB_TOKN_URL, "https://github.com/login/oauth/access_token?").
+
 
 -ifdef(DEVELOP).
--define(GITHUB_AUTH_CB_URL, <<"http://127.0.0.1:8000/50pm/api/login/github">>).
--elifdef(STAGING).
--define(GITHUB_AUTH_CB_URL, <<"http://dev.davidhuang.top/50pm/api/login/github">>).
+    -define(GITHUB_OAUTH_CB_URL, "http://127.0.0.1:8000/50pm/api/login/github/callback").
+    -define(APP_URL, "http://127.0.0.1:8080/50pm").
 -else.
--define(GITHUB_AUTH_CB_URL, <<"GITHUB_AUTH_CB_URL not properly set">>).
+    -ifdef(STAGING).
+        -define(GITHUB_OAUTH_CB_URL, "https://dev.davidhuang.top/50pm/api/login/github/callback").
+        -define(APP_URL, "https://dev.davidhuang.top/50pm").
+    -else.
+        -define(GITHUB_OAUTH_CB_URL, "GITHUB_OAUTH_CB_URL not properly set").
+        -define(APP_URL, "APP_URL not properly set").
+    -endif.
 -endif.
 
 %%====================================================================
@@ -21,21 +28,55 @@
 %%====================================================================
 
 init(R, S) ->
-    ExtAuth = cowboy_req:binding(oidp, R),
-    handle(ExtAuth, R, S).
+    PL = cowboy_req:path_info(R),
+    handle(PL, R, S).
 
-handle(<<"github">>, R, S) ->
+handle([Provider], R, S) -> oauth_start(Provider, R, S);
+handle([Provider, <<"callback">>], R, S) -> 
+    #{code := Code, state := State} 
+        = cowboy_req:match_qs(
+            [
+                {code, nonempty, undefined}, 
+                {state, int, undefined}
+            ],
+            R
+        ),
+    oauth_callback(Provider, Code, State, R, S).
+
+oauth_start(<<"github">>, R, S) ->
     Q = build_qs(
         #{
             response_type => "code",
             client_id => client_id(),
-            redirect_uri => ?GITHUB_AUTH_CB_URL,
-            scope => "user public_repo",
-            state => "0123456789abcdef"
-        }),
+            redirect_uri => ?GITHUB_OAUTH_CB_URL,
+            scope => "user",
+            state => integer_to_list(gen_state(github, state_timeout()))
+        }
+    ),
+    AuthURL = ?GITHUB_AUTH_URL ++ Q,
+    httpres:'302'(AuthURL, R, S).
 
-    AuthURL = ?GITHUB_AUTH_URL_PRE ++ Q,
-    httpres:'200'(AuthURL, R, S).
+oauth_callback(_Provider, undefined, _State, R, S) -> httpres:'400'("no code", R, S);
+oauth_callback(_Provider, _Code, undefined, R, S) -> httpres:'400'("bad state", R, S);
+oauth_callback(<<"github">>, Code, State, R, S) ->
+    Q = build_qs(
+        #{
+            client_id => client_id(),
+            client_secret => client_secret(),
+            code => binary_to_list(Code),
+            redirect_uri => ?GITHUB_OAUTH_CB_URL,
+            state => integer_to_list(State)
+        }
+    ),
+    TokenURL = ?GITHUB_TOKN_URL ++ Q,
+    {ok, {{_, 200, _}, Headers, Body}} = httpc:request(post, {TokenURL, [], [], []}, [], []),
+    io:format("~noauth_callback Headers ~p", [Headers]),
+    io:format("~noauth_callback Body ~p~n", [Body]),
+    httpres:'302'(?APP_URL, R, S).
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
 
 build_qs(M) ->
     PL = maps:to_list(M),
@@ -44,8 +85,18 @@ build_qs(M) ->
             PL),
     QS = lists:join("&", SL),
     lists:flatten(QS).
-%%====================================================================
-%% Internal functions
-%%====================================================================
 
-client_id() -> application:get_env(fiftypm_api, client_id).
+client_id() -> client_id(application:get_env(fiftypm_api, client_id)).
+client_id({ok, ClientId}) -> ClientId.
+
+client_secret() -> client_secret(application:get_env(fiftypm_api, client_secret)).
+client_secret({ok, ClientSecret}) -> ClientSecret.
+
+gen_state(V, TimeOut) -> 
+    StateInt = rand:uniform(1000000),
+    tab_kv:dset(StateInt, {oauth_state, V}),
+    _Pid = spawn(fun() -> timer:sleep(TimeOut), tab_kv:ddel(StateInt) end),
+    StateInt.
+    
+-define(DEFAULT_STATE_TIMEOUT, (10*1000)).
+state_timeout() -> application:get_env(fiftypm_api, state_timeout, ?DEFAULT_STATE_TIMEOUT).
--- backend/fiftypm_api/src/httpres.erl
+++ backend/fiftypm_api/src/httpres.erl
@@ -1,8 +1,13 @@
 -module(httpres).
 
--export(['200'/2, '200'/3, '200'/4,
-         '400'/2, '400'/3, '400'/4,
-         '404'/2, '404'/3, '404'/4]).
+-export(
+    [
+        '200'/2, '200'/3, '200'/4,
+        '302'/3,
+        '400'/2, '400'/3, '400'/4,
+        '404'/2, '404'/3, '404'/4
+    ]
+).
 
 -define(CT_TEXT_TEXT, <<"text/text">>).
 -define(CT_APPLICATION_JSON, <<"application/json">>).
@@ -14,6 +19,8 @@
 '200'(R, S) -> reply(200, R, S).
 '200'(T, R, S) -> reply(200, T, R, S).
 '200'(json, T, R, S) -> reply(200, ?CT_APPLICATION_JSON, T, R, S).
+
+'302'(L, R, S) -> redirect(302, L, R, S).
 
 '400'(R, S) -> reply(400, R, S).
 '400'(T, R, S) -> reply(400, T, R, S).
@@ -30,3 +37,5 @@
 reply(C, R, S) -> {ok, cowboy_req:reply(C, R), S}.
 reply(C, T, R, S) -> reply(C, ?CT_TEXT_TEXT, T, R, S).
 reply(C, CT, T, R, S) -> {ok, cowboy_req:reply(C, #{<<"content-type">> => CT}, T, R), S}.
+
+redirect(C, L, R, S) -> {ok, cowboy_req:reply(C, #{<<"location">> => L}, R), S}.
