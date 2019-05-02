25affc71dc854a6b2d0790e2b0a00b83045a8074
Yong Huang <yong.huang.sh@gmail.com>
2019-05-02 23:16:20 +0800

    Make tab_kv accept table name

--- backend/fiftypm_api/src/fiftypm_api_app.erl
+++ backend/fiftypm_api/src/fiftypm_api_app.erl
@@ -108,9 +108,7 @@ start_mnesia_init_tab(Np) when is_atom(Np) ->
     SchemaStorageType1 = mnesia:table_info(schema, storage_type),
     init_tab_n(SchemaStorageType1).
 
-init_tab_0(ok) -> 
-    {atomic, ok} = tab_session:create_table(),
-    {atomic, ok} = tab_kv:create_table();
+init_tab_0(ok) -> {atomic, ok} = tab_kv:create_table(kv_tables());
 init_tab_0({error, {_,{already_exists, _}}}) -> {atomic, ok}.
 
 add_this_node(ram_copies, Np) -> {ok, _} = mnesia:change_config(extra_db_nodes, [Np]);
@@ -118,9 +116,10 @@ add_this_node(disc_copies, _) -> {ok, ignored}.
 
 init_tab_n(ram_copies) -> 
     {atomic, ok} = mnesia:change_table_copy_type(schema, node(), disc_copies),
-    {atomic, ok} = tab_session:add_table_copy(),
-    {atomic, ok} = tab_kv:add_table_copy();
+    {atomic, ok} = tab_kv:add_table_copy(kv_tables());
 init_tab_n(disc_copies) -> {atomic, ok}.
+
+kv_tables() -> [oauth_state, session].
 
 %%
 start_cowboy(S) ->
