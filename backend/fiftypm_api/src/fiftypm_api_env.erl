9460b94bfd1ab92e4c4a4f7b49ab86d1d961522b
Yong Huang <yong.huang.sh@gmail.com>
2019-05-02 23:16:42 +0800

    Clean up and move more env to this file

--- backend/fiftypm_api/src/fiftypm_api_env.erl
+++ backend/fiftypm_api/src/fiftypm_api_env.erl
@@ -1,12 +1,13 @@
 -module(fiftypm_api_env).
 
--behaviour(application).
-
 %% Application callbacks
 -export(
     [
         prefix/1,
-        client_id/0, client_secret/0,
+        
+        oauth_client_id/1, 
+        oauth_client_secret/1,
+        oauth_state_domain/0,
         oauth_state_timeout/0
     ]
 ).
@@ -15,6 +16,7 @@
 %% Macros
 %%====================================================================
 
+-define(DEFAULT_STATE_DOMAIN, 1000000000).
 -define(DEFAULT_OAUTH_STATE_TIMEOUT, (10*1000)).
 
 %%====================================================================
@@ -23,10 +25,11 @@
 
 prefix(Path) -> application:get_env(fiftypm_api, prefix, "") ++ Path.
 
-client_id() -> client_id(application:get_env(fiftypm_api, client_id)).
-client_id({ok, ClientId}) -> ClientId.
+oauth_client_id(github) -> oauth_client_id(application:get_env(fiftypm_api, oauth_github_client_id));
+oauth_client_id({ok, ClientId}) -> ClientId.
 
-client_secret() -> client_secret(application:get_env(fiftypm_api, client_secret)).
-client_secret({ok, ClientSecret}) -> ClientSecret.
+oauth_client_secret(github) -> oauth_client_secret(application:get_env(fiftypm_api, oauth_github_client_secret));
+oauth_client_secret({ok, ClientSecret}) -> ClientSecret.
 
+oauth_state_domain() -> application:get_env(fiftypm_api, oauth_state_domain, ?DEFAULT_STATE_DOMAIN).
 oauth_state_timeout() -> application:get_env(fiftypm_api, oauth_state_timeout, ?DEFAULT_OAUTH_STATE_TIMEOUT).
